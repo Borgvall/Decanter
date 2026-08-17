9b6915a452f8bf1e6d2577e00bfcb4107323af22
Johannes Röhl <joroehl@gmx.de>
2026-08-17 22:44:22 +0200

    Fix: use channel as flake input

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Decanter - A modern Wine prefix manager";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.xz";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
